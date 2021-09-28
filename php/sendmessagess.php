c2526aeaf1524a87eb6cd4be99d72b48867ba862
Rudy0102 <robertzoltowski0102@gmail.com>
2021-09-28 19:20:35 +0200

    sendmessages dziala

--- php/sendmessagess.php
+++ php/sendmessagess.php
@@ -1,9 +1,9 @@
 <?php
-var_dump($_POST);
+if  (session_status() !== PHP_SESSION_ACTIVE) session_start();
 if (isset($_POST["usertext"])){
     $usertext = $_POST['usertext'];
     $data = date("Y-m-d H:i:s");
-    $question = "INSERT INTO wiadomosci (autor, tresc, data) VALUES ('test', '$usertext', '$data')";
+    $question = "INSERT INTO wiadomosci (autor, tresc, data) VALUES (".$_SESSION['userid'].", '$usertext', '$data')";
     require("./connectdatabase.php");
     $result = mysqli_query($link, $question);
     mysqli_close($link);
