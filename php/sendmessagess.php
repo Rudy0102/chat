adff2ee43d60390b6d45b92ede7e0eba3fd2decb
Rudy0102 <robertzoltowski0102@gmail.com>
2021-09-28 20:40:29 +0200

    working

--- php/sendmessagess.php
+++ php/sendmessagess.php
@@ -1,10 +1,10 @@
 <?php
 if  (session_status() !== PHP_SESSION_ACTIVE) session_start();
-var_dump($_POST);
-echo $_POST["usertext"];
-$_SESSION['userid']=1;
+// var_dump($_POST);
+// echo $_POST["usertext"];
+// $_SESSION['userid']=1;
 if  (isset($_POST["usertext"])){
-    echo "test";
+    // echo "test";
     $usertext = $_POST['usertext'];
     $data = date("Y-m-d H:i:s");
     $question = "INSERT INTO wiadomosci (autor, tresc, data) VALUES (".$_SESSION['userid'].", '$usertext', '$data')";
